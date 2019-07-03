8f0880201b17dcb3458c130aaebae0f41c16e6e2
Name <dmitry.taran333@mail.ru>
2019-07-03 18:44:16 +0300

    add new method

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,10 @@
 module javaFX {
-    requires  javafx.fxml;
+    requires javafx.fxml;
     requires javafx.controls;
     requires org.apache.commons.codec;
     requires java.desktop;
+    requires java.security.jgss;
+    requires java.prefs;
 
     opens sample;
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,85 +1,133 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import org.apache.commons.codec.binary.Hex;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.awt.*;
 import java.awt.datatransfer.*;
 import java.util.Random;
+import java.util.prefs.Preferences;
 
 
 public class Main extends Application {
 
-    private static final String INIT_ARRAY = "ItIsOutBigSecret";
+    protected static final String INIT_TEXT = "ItIsOutBigSecret";
     private static boolean bufferChanged = false;
+    private static boolean checkPassiveEncryption = false;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("windows/mainWindow.fxml"));
         primaryStage.setTitle("noRead");
         Scene scene = new Scene(root, 808, 606);
         primaryStage.setScene(scene);
         primaryStage.show();
         listener();
-    }
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                Platform.exit();
+            }
+        });
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
     protected synchronized static String encryption(String word, String key) throws Exception {
+
+        // create quantity
         Random random = new Random();
         int quantity = random.nextInt(9) + 1;
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(INIT_ARRAY.getBytes("UTF-8"));
+
+        // create encryption
+        key = encryptionKey(key);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(INIT_TEXT.getBytes("UTF-8"));
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+        // encryption text
         for (int i = 0; i < quantity; i++) {
             word = new String(Hex.encodeHex(cipher.doFinal(word.getBytes("UTF-8")), false));
         }
-        return word + quantity;
+        return word + quantity + "/e";
     }
 
     protected synchronized static String decryption(String word, String key) throws Exception {
+
+        // read quantity
         int length = word.length();
-        int quantity = Integer.parseInt(Character.toString(word.charAt(length - 1)));
-        word = word.substring(0, length - 1);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(INIT_ARRAY.getBytes("UTF-8"));
+        int quantity = Integer.parseInt(Character.toString(word.charAt(length - 3)));
+        word = word.substring(0, length - 3);
+
+        // create decryption
+        key = encryptionKey(key);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(INIT_TEXT.getBytes("UTF-8"));
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+        // decryption text
         for (int i = 0; i < quantity; i++) {
             word = new String(cipher.doFinal(Hex.decodeHex(word.toCharArray())));
         }
         return word;
     }
 
-    protected static void listener() {
+    private static void listener() {
         Clipboard clipboardListener = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboardListener.addFlavorListener(new FlavorListener() {
 
             @Override
             public synchronized void flavorsChanged(FlavorEvent e) {
-                if (bufferChanged){
-                    bufferChanged = false;
-                    return;
-                }
-                try {
-                    String text = (String) clipboardListener.getContents(null).getTransferData(DataFlavor.stringFlavor);
-                    text = encryption(text, "йцуйцуйц");
-                    StringSelection stringSelection = new StringSelection(text);
-                    clipboardListener.setContents(stringSelection, null);
-                    bufferChanged = true;
-                } catch (Exception e1) {
+                if (checkPassiveEncryption) {
+                    System.out.println(1);
+                    if (bufferChanged) {
+                        bufferChanged = false;
+                        return;
+                    }
+                    try {
+                        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+                        String key = prefs.get("keyForPassiveMod", null);
+                        key = key == null ? INIT_TEXT : key;
+                        String text = (String) clipboardListener.getContents(null).getTransferData(DataFlavor.stringFlavor);
+                        if (text.substring(text.length() - 2).equals("/e")) {
+                            text = decryption(text, key);
+                        } else {
+                            text = encryption(text, key);
+                        }
+                        StringSelection stringSelection = new StringSelection(text);
+                        clipboardListener.setContents(stringSelection, null);
+                        bufferChanged = true;
+                    } catch (Exception e1) {
+                    }
                 }
             }
         });
+    }
+
+    private static String encryptionKey(String key) throws Exception {
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(INIT_TEXT.getBytes("UTF-8"));
+        SecretKeySpec secretKeySpec = new SecretKeySpec(INIT_TEXT.getBytes("UTF-8"), "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+        return new String(Hex.encodeHex(cipher.doFinal(key.getBytes("UTF-8")), false)).substring(0, 32);
+    }
+
+    protected static void isPassiveEncryption(boolean answer) {
+        checkPassiveEncryption = answer;
     }
 }
